f79557a48f49608e339c6965fab191d7af8496ed
hoan99 <tuoimeo.99@gmail.com>
2023-04-03 23:53:59 +0700

    ConstraintLayout

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -4,56 +4,77 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.delay
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val items = remember {
-                mutableStateListOf("")
+            var username by remember {
+                mutableStateOf("")
             }
-            LaunchedEffect(key1 = true) {
-                delay(2000)
-                repeat(20) {
-                    items.add("Number: $it")
-                }
+            ConstraintView(
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .fillMaxSize(),
+                username = username
+            ) {
+                username = it
             }
-            ListItem(items = items)
         }
     }
 }
 
 @Composable
-fun ListItem(
-    items: List<String>
+fun ConstraintView(
+    modifier: Modifier = Modifier,
+    username: String,
+    onUsernameChanged: (String) -> Unit,
 ) {
-    LazyColumn(
-        modifier = Modifier.fillMaxSize()
+    val constraints = ConstraintSet {
+        val tfUsername = createRefFor("tfUsername")
+        val btnConfirm = createRefFor("btnConfirm")
+        constrain(tfUsername) {
+            top.linkTo(parent.top)
+            bottom.linkTo(parent.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(btnConfirm.start, 10.dp)
+            width = Dimension.fillToConstraints
+            height = Dimension.wrapContent
+        }
+        constrain(btnConfirm) {
+            top.linkTo(tfUsername.top)
+            bottom.linkTo(tfUsername.bottom)
+            end.linkTo(parent.end)
+            width = Dimension.wrapContent
+            height = Dimension.wrapContent
+        }
+    }
+    ConstraintLayout(
+        constraintSet = constraints,
+        modifier = modifier,
     ) {
-        items(items) {
-            Text(
-                text = it,
-                fontSize = 30.sp,
-                textAlign = TextAlign.Justify,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(20.dp)
-            )
+        OutlinedTextField(
+            value = username,
+            onValueChange = onUsernameChanged,
+            modifier = Modifier.layoutId("tfUsername")
+        )
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.layoutId("btnConfirm")
+        ) {
+            Text(text = "Confirm")
         }
     }
 }
