fd2bb34e215d773f9d90d4007182f0936f0f2513
hoan99 <tuoimeo.99@gmail.com>
2023-04-03 22:57:41 +0700

    Textfields, Buttons & Showing Snackbars

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -3,56 +3,88 @@ package com.example.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import kotlin.random.Random
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var color by remember {
-                mutableStateOf(Color.Black)
+            var username by remember {
+                mutableStateOf("")
             }
-            Column {
-                ColorBox(
-                    modifier = Modifier
-                        .fillMaxHeight(0.7f)
-                        .fillMaxWidth(),
-                    color = color,
-                )
-                Spacer(modifier = Modifier.height(50.dp))
-                Box(
-                    modifier = Modifier
-                        .background(Color.Red)
-                        .fillMaxSize()
-                        .clickable {
-                            color = Color(
-                                Random.nextFloat(),
-                                Random.nextFloat(),
-                                Random.nextFloat(),
-                                1f,
-                            )
-                        }
-                )
+            val snackbarHostState = remember {
+                SnackbarHostState()
             }
-
+            var confirmedState by remember {
+                mutableStateOf(false)
+            }
+            LoginForm(
+                modifier = Modifier.fillMaxSize(),
+                snackbarHostState = snackbarHostState,
+                username = username,
+                onUsernameChanged = {
+                    username = it
+                    confirmedState = false
+                },
+                onConfirmed = {
+                    confirmedState = true
+                },
+                isConfirmClicked = confirmedState,
+            )
         }
     }
 }
 
 @Composable
-fun ColorBox(
+fun LoginForm(
     modifier: Modifier = Modifier,
-    color: Color,
+    snackbarHostState: SnackbarHostState,
+    username: String,
+    onUsernameChanged: (String) -> Unit,
+    onConfirmed: () -> Unit,
+    isConfirmClicked: Boolean
 ) {
-    Box(
-        modifier = modifier
-            .background(color = color),
-    )
+    LaunchedEffect(isConfirmClicked, username) {
+        if (isConfirmClicked && username.isNotEmpty()) {
+            snackbarHostState.showSnackbar(
+                "Hello $username"
+            )
+        }
+    }
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = 20.dp)
+                .fillMaxSize()
+                .wrapContentSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.End,
+        ) {
+            OutlinedTextField(
+                value = username,
+                onValueChange = onUsernameChanged,
+                modifier = Modifier.fillMaxWidth(),
+                label = {
+                    Text(text = "Enter username")
+                },
+                singleLine = true,
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            OutlinedButton(onClick = onConfirmed) {
+                Text(text = "Confirm")
+            }
+        }
+    }
 }
