d5d11db507637de9a8fe316843acd517a9b0b36a
hoan.nt <hoan.nguyenthanh@vti.com.vn>
2023-04-18 17:37:32 +0700

    movable pieces

--- app/src/main/java/com/example/composepractice/MainActivity.kt
+++ app/src/main/java/com/example/composepractice/MainActivity.kt
@@ -1,74 +1,57 @@
 package com.example.composepractice
 
 import android.os.Bundle
-import android.util.Log
-import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.PathFillType
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.rotate
-import androidx.compose.ui.input.pointer.pointerInteropFilter
-import androidx.compose.ui.layout.boundsInParent
-import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.drawText
-import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.example.composepractice.ui.features.Board
 import com.example.composepractice.ui.features.Cell
 import com.example.composepractice.ui.features.CellState
-import com.example.composepractice.ui.features.login.LoginViewModel
-import kotlin.math.PI
-import kotlin.math.abs
-import kotlin.math.atan2
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BoxWithConstraints(
+            Column(
                 modifier = Modifier
                     .fillMaxSize(),
-                contentAlignment = Alignment.Center,
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 var board by remember {
                     mutableStateOf(Board())
                 }
-
+                Text(
+                    text = if (board.currentTurn == Board.WhiteTurn) "White turn"
+                    else "Black turn",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                )
                 BoardComponent(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(this.maxWidth)
+                        .aspectRatio(1f)
                         .background(Color(0xE6FDD458)),
                     board = board,
                 ) { row, col ->
@@ -209,6 +192,7 @@ fun BoardComponent(
                                 .fillMaxHeight()
                                 .fillMaxWidth(1f.div(5 - col)),
                             cell = cell,
+                            isSelected = board.selectedCell?.id == cell.id,
                         ) {
                             onCellSelected.invoke(row, col)
                         }
@@ -223,32 +207,65 @@ fun BoardComponent(
 fun CellComponent(
     modifier: Modifier = Modifier,
     cell: Cell,
+    isSelected: Boolean,
     onPieceSelected: () -> Unit
 ) {
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
     Box(
         modifier = modifier
             .background(Color.Transparent)
-            .clickable(onClick = onPieceSelected),
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = onPieceSelected,
+            ),
         contentAlignment = Alignment.Center,
     ) {
+        @Composable
+        fun drawHint(state: CellState) {
+            val hintModifier = Modifier.alpha(0.5f)
+            when (state) {
+                CellState.WHITE_PAWN_OCCUPIED -> PawnPiece(
+                    modifier = hintModifier,
+                    color = Color.White,
+                )
+                CellState.WHITE_KING_OCCUPIED -> KingPiece(
+                    modifier = hintModifier,
+                    color = Color.White,
+                )
+                CellState.BLACK_PAWN_OCCUPIED -> PawnPiece(
+                    modifier = hintModifier,
+                    color = Color.Black,
+                )
+                CellState.BLACK_KING_OCCUPIED -> KingPiece(
+                    modifier = hintModifier,
+                    color = Color.Black,
+                )
+                else -> {}
+            }
+        }
         when (cell.state) {
             CellState.WHITE_PAWN_OCCUPIED -> PawnPiece(
                 color = Color.White,
-                selected = cell.selected
+                selected = isSelected,
             )
             CellState.WHITE_KING_OCCUPIED -> KingPiece(
                 color = Color.White,
-                selected = cell.selected
+                selected = isSelected,
             )
             CellState.BLACK_PAWN_OCCUPIED -> PawnPiece(
                 color = Color.Black,
-                selected = cell.selected
+                selected = isSelected,
             )
             CellState.BLACK_KING_OCCUPIED -> KingPiece(
                 color = Color.Black,
-                selected = cell.selected
+                selected = isSelected,
             )
-            else -> {}
+            else -> {
+                drawHint(state = cell.hintState)
+            }
         }
     }
 }
@@ -257,17 +274,26 @@ fun CellComponent(
 fun PawnPiece(
     modifier: Modifier = Modifier,
     color: Color,
-    selected: Boolean,
+    selected: Boolean = false,
 ) {
-    val size = 20.dp + (if (selected) 10.dp else 0.dp)
+    val size by remember {
+        mutableStateOf(20.dp)
+    }
+    val animatedSize by animateDpAsState(
+        targetValue = size + (if (selected) 10.dp else 0.dp),
+        animationSpec = tween(
+            durationMillis = 500,
+        )
+    )
     Canvas(
         modifier = modifier
             .shadow(
                 elevation = if (selected) 5.dp else 0.dp,
                 spotColor = Color.Red,
             )
-            .size(size, size),
+            .size(animatedSize, animatedSize),
     ) {
+        20.dp.toPx()
         drawCircle(
             color = color,
         )
@@ -278,290 +304,27 @@ fun PawnPiece(
 fun KingPiece(
     modifier: Modifier = Modifier,
     color: Color,
-    selected: Boolean,
+    selected: Boolean = false,
 ) {
-    val size = 40.dp + (if (selected) 20.dp else 0.dp)
+    val size by remember {
+        mutableStateOf(40.dp)
+    }
+    val animatedSize by animateDpAsState(
+        targetValue = size + (if (selected) 20.dp else 0.dp),
+        animationSpec = tween(
+            durationMillis = 500,
+        )
+    )
     Canvas(
         modifier = modifier
             .shadow(
                 elevation = if (selected) 5.dp else 0.dp,
                 spotColor = Color.Red,
             )
-            .size(size, size),
+            .size(animatedSize, animatedSize),
     ) {
         drawCircle(
             color = color,
         )
-    }
-}
-
-@OptIn(ExperimentalComposeUiApi::class, ExperimentalTextApi::class)
-@Composable
-fun GestureCheck() {
-    var rect by remember {
-        mutableStateOf(
-            Rect(0f, 0f, 0f, 0f)
-        )
-    }
-    var centerX by remember {
-        mutableStateOf(0f)
-    }
-    var centerY by remember {
-        mutableStateOf(0f)
-    }
-    var pointerX by remember {
-        mutableStateOf(0f)
-    }
-    var pointerY by remember {
-        mutableStateOf(0f)
-    }
-    var angle by remember {
-        mutableStateOf(60f)
-    }
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .pointerInteropFilter { event ->
-                when (event.action) {
-                    MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
-                        pointerX = event.x
-                        pointerY = event.y
-                        angle =
-                            atan2(centerY - pointerY, pointerX - centerX)
-                                .div(PI)
-                                .times(180)
-                                .toFloat()
-                        true
-                    }
-                    else -> false
-                }
-            },
-        contentAlignment = Alignment.Center,
-    ) {
-        Box(
-            modifier = Modifier
-                .size(100.dp)
-                .background(Color.Green)
-                .onGloballyPositioned {
-                    rect = it.boundsInParent()
-                    centerX = rect.center.x
-                    centerY = rect.center.y
-                }
-                .rotate(angle)
-
-        ) {
-
-        }
-        val textMeasurer = rememberTextMeasurer()
-        Canvas(
-            modifier = Modifier.fillMaxSize(),
-        ) {
-            rotate(-angle) {
-                drawRect(
-                    color = Color.Red,
-                    topLeft = rect.topLeft,
-                    size = rect.size.times(1 + angle / 360),
-                    alpha = 0.75f,
-                    style = Stroke(width = 3.dp.toPx()),
-                )
-            }
-            drawCircle(
-                color = Color.Black,
-                radius = 5.dp.toPx(),
-                center = Offset(centerX, centerY),
-            )
-            drawCircle(
-                color = Color.Black,
-                radius = 5.dp.toPx(),
-                center = Offset(pointerX, pointerY),
-            )
-            drawLine(
-                color = Color.Black,
-                start = Offset(centerX, centerY),
-                end = Offset(pointerX, pointerY),
-            )
-            drawText(
-                textMeasurer = textMeasurer,
-                text = angle.toString(),
-                topLeft = Offset(pointerX + 10.dp.toPx(), pointerY + 10.dp.toPx()),
-            )
-        }
-    }
-}
-
-@Composable
-fun NavGraph() {
-    val navController = rememberNavController()
-    NavHost(
-        navController = navController,
-        startDestination = "constraint",
-    ) {
-        composable("constraint") {
-            var selectedPhase by remember {
-                mutableStateOf("")
-            }
-            ConstraintView(
-                modifier = Modifier
-                    .padding(horizontal = 20.dp)
-                    .fillMaxHeight(0.3f)
-                    .fillMaxWidth(),
-                username = selectedPhase,
-                onUsernameChanged = {
-                    selectedPhase = it
-                },
-                onConfirmed = {
-                    if (selectedPhase.isEmpty()) return@ConstraintView
-                    navController.navigate("drawing")
-                }
-            )
-        }
-        composable(
-            route = "drawing?startPhase={startPhase}",
-            arguments = listOf(
-                navArgument("startPhase") {
-                    defaultValue = "0"
-                }
-            ),
-        ) { entry ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize(),
-
-                ) {
-                var phase by remember {
-                    mutableStateOf(
-                        entry.arguments?.getString("startPhase")?.toIntOrNull() ?: -100
-                    )
-                }
-                Drawing(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(0.8f)
-                        .align(Alignment.CenterHorizontally),
-                    phase = phase,
-                )
-                Slider(
-                    value = phase.toFloat(),
-                    valueRange = -100f..100f,
-                    onValueChange = {
-                        phase = it.toInt()
-                    }
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun Drawing(
-    modifier: Modifier = Modifier,
-    phase: Int,
-) {
-    val path = remember {
-        Path()
-    }
-    Canvas(
-        modifier = modifier.background(Color.Black)
-    ) {
-        path.reset()
-        val radius = 500f
-        drawCircle(
-            color = Color.White,
-            radius = radius,
-        )
-
-        val oWidth = radius.times(2).div(100).times(abs(phase))
-        val oHeight = radius.times(2)
-        val x = size.width.div(2) - oWidth.div(2)
-        val y = size.height.div(2) - radius
-        path.arcTo(
-            rect = Rect(
-                offset = Offset(
-                    size.width.div(2) - radius, y
-                ),
-                size = Size(width = oHeight, height = oHeight)
-            ),
-            startAngleDegrees = 90f,
-            sweepAngleDegrees = 180f,
-            forceMoveTo = true,
-        )
-        val startAngle = if (phase <= 0) 90f else 270f
-        path.arcTo(
-            rect = Rect(
-                offset = Offset(
-                    x = x,
-                    y = y,
-                ),
-                size = Size(oWidth, oHeight),
-            ),
-            startAngleDegrees = startAngle,
-            sweepAngleDegrees = 180f,
-            forceMoveTo = true
-        )
-        path.fillType = PathFillType.EvenOdd
-        drawPath(
-            path = path,
-            color = Color.Black,
-            alpha = 0.25f,
-        )
-
-    }
-}
-
-@Composable
-fun ConstraintView(
-    modifier: Modifier = Modifier,
-    username: String,
-    onUsernameChanged: (String) -> Unit,
-    onConfirmed: () -> Unit,
-    viewModel: LoginViewModel = viewModel()
-) {
-    val (a, setA) = remember {
-        mutableStateOf("")
-    }
-    var userName by viewModel.usernameState
-    val constraints = ConstraintSet {
-        val tfUsername = createRefFor("tfUsername")
-        val btnConfirm = createRefFor("btnConfirm")
-        constrain(tfUsername) {
-            top.linkTo(parent.top)
-            bottom.linkTo(parent.bottom)
-            start.linkTo(parent.start)
-            end.linkTo(btnConfirm.start, 10.dp)
-            width = Dimension.fillToConstraints
-            height = Dimension.wrapContent
-        }
-        constrain(btnConfirm) {
-            top.linkTo(tfUsername.top)
-            bottom.linkTo(tfUsername.bottom)
-            end.linkTo(parent.end)
-            width = Dimension.wrapContent
-            height = Dimension.wrapContent
-        }
-    }
-    ConstraintLayout(
-        constraintSet = constraints,
-        modifier = modifier,
-    ) {
-        OutlinedTextField(
-            value = userName,
-//            onValueChange = onUsernameChanged,
-            onValueChange = { userName = it },
-            modifier = Modifier.layoutId("tfUsername"),
-            label = {
-                Text(
-                    fontSize = 12.sp,
-                    text = "Enter a number between -100 and 100"
-                )
-            }
-        )
-        OutlinedButton(
-            onClick = {
-                Log.d("TAG", "ConstraintView: ${viewModel.usernameState.value}")
-            },
-            modifier = Modifier.layoutId("btnConfirm")
-        ) {
-            Text(text = "Confirm")
-        }
     }
 }
--- app/src/main/java/com/example/composepractice/ui/features/PH.kt
+++ app/src/main/java/com/example/composepractice/ui/features/PH.kt
@@ -1,53 +1,162 @@
 package com.example.composepractice.ui.features
 
 class Board(
-    val cells: Array<Array<Cell>> = Array(5) {
-        Array(5) {
-            Cell()
+    val cells: Array<Array<Cell>> = Array(BoardSize) { row ->
+        Array(BoardSize) { col ->
+            if (row.rem(2) == col.rem(2)) {
+                Cell.EightDirectionCell(id = row to col)
+            } else {
+                Cell.FourDirectionCell(id = row to col)
+            }
         }
     },
+    selectedCell: Cell? = null,
+    initPieces: Boolean = true,
+    var currentTurn: Int = WhiteTurn,
 ) {
-    init {
-        cells[0][0].state = CellState.WHITE_PAWN_OCCUPIED
-        cells[0][1].state = CellState.WHITE_PAWN_OCCUPIED
-        cells[0][2].state = CellState.WHITE_KING_OCCUPIED
-        cells[0][3].state = CellState.WHITE_PAWN_OCCUPIED
-        cells[0][4].state = CellState.WHITE_PAWN_OCCUPIED
-        cells[1][0].state = CellState.WHITE_PAWN_OCCUPIED
-        cells[1][1].state = CellState.WHITE_PAWN_OCCUPIED
-        cells[1][3].state = CellState.WHITE_PAWN_OCCUPIED
-        cells[1][4].state = CellState.WHITE_PAWN_OCCUPIED
-        cells[2][0].state = CellState.WHITE_PAWN_OCCUPIED
+    companion object {
+        const val BoardSize = 5
+        const val WhiteTurn = 0
+        const val BlackTurn = 1
+    }
 
-        cells[4][0].state = CellState.BLACK_PAWN_OCCUPIED
-        cells[4][1].state = CellState.BLACK_PAWN_OCCUPIED
-        cells[4][2].state = CellState.BLACK_KING_OCCUPIED
-        cells[4][3].state = CellState.BLACK_PAWN_OCCUPIED
-        cells[4][4].state = CellState.BLACK_PAWN_OCCUPIED
-        cells[3][0].state = CellState.BLACK_PAWN_OCCUPIED
-        cells[3][1].state = CellState.BLACK_PAWN_OCCUPIED
-        cells[3][3].state = CellState.BLACK_PAWN_OCCUPIED
-        cells[3][4].state = CellState.BLACK_PAWN_OCCUPIED
-        cells[2][4].state = CellState.BLACK_PAWN_OCCUPIED
+    var selectedCell: Cell? = selectedCell
+        private set
+
+    init {
+        if (initPieces) {
+            cells[0][0].state = CellState.WHITE_PAWN_OCCUPIED
+            cells[0][1].state = CellState.WHITE_PAWN_OCCUPIED
+            cells[0][2].state = CellState.WHITE_KING_OCCUPIED
+            cells[0][3].state = CellState.WHITE_PAWN_OCCUPIED
+            cells[0][4].state = CellState.WHITE_PAWN_OCCUPIED
+            cells[1][0].state = CellState.WHITE_PAWN_OCCUPIED
+            cells[1][1].state = CellState.WHITE_PAWN_OCCUPIED
+            cells[1][3].state = CellState.WHITE_PAWN_OCCUPIED
+            cells[1][4].state = CellState.WHITE_PAWN_OCCUPIED
+            cells[2][0].state = CellState.WHITE_PAWN_OCCUPIED
+
+            cells[4][0].state = CellState.BLACK_PAWN_OCCUPIED
+            cells[4][1].state = CellState.BLACK_PAWN_OCCUPIED
+            cells[4][2].state = CellState.BLACK_KING_OCCUPIED
+            cells[4][3].state = CellState.BLACK_PAWN_OCCUPIED
+            cells[4][4].state = CellState.BLACK_PAWN_OCCUPIED
+            cells[3][0].state = CellState.BLACK_PAWN_OCCUPIED
+            cells[3][1].state = CellState.BLACK_PAWN_OCCUPIED
+            cells[3][3].state = CellState.BLACK_PAWN_OCCUPIED
+            cells[3][4].state = CellState.BLACK_PAWN_OCCUPIED
+            cells[2][4].state = CellState.BLACK_PAWN_OCCUPIED
+        }
     }
 
     fun selectCell(row: Int, col: Int): Board {
-        cells[row][col].apply {
-            selected = !selected
+        val newSelectedCell = cells[row][col]
+        if (newSelectedCell.hintState != CellState.EMPTY) {
+            this.selectedCell ?: return this
+            removeHint()
+            newSelectedCell.state = this.selectedCell!!.state
+            this.selectedCell!!.state = CellState.EMPTY
+            this.selectedCell = null
+            this.currentTurn = if (this.currentTurn == WhiteTurn) BlackTurn else WhiteTurn
+            return this.copy()
         }
-        return Board(cells = this.cells)
+        if (newSelectedCell.state.type != currentTurn) return this
+        when (selectedCell?.id) {
+            newSelectedCell.id -> {
+                selectedCell = null
+                removeHint(newSelectedCell)
+            }
+            else -> {
+                if (selectedCell != null) {
+                    removeHint()
+                }
+                selectedCell = newSelectedCell
+                showHint(newSelectedCell)
+            }
+        }
+        return this.copy()
     }
+
+    private fun removeHint(
+        cell: Cell? = selectedCell,
+        movements: List<CellMovement> = cell?.getPossibleMovements() ?: emptyList()
+    ) {
+        cell ?: return
+        movements.forEach { move ->
+            val movableCell =
+                this.cells[cell.id.first + move.direction.first][cell.id.second + move.direction.second]
+            movableCell.hintState = CellState.EMPTY
+        }
+    }
+
+    private fun showHint(
+        cell: Cell? = this.selectedCell,
+        movements: List<CellMovement> = cell?.getPossibleMovements() ?: emptyList()
+    ) {
+        cell ?: return
+        movements.forEach { move ->
+            val movableCell =
+                this.cells[cell.id.first + move.direction.first][cell.id.second + move.direction.second]
+            movableCell.hintState = cell.state
+        }
+    }
+
+    private fun Cell.getPossibleMovements(): List<CellMovement> {
+        val movements = when (this) {
+            is Cell.FourDirectionCell -> arrayOf(
+                CellMovement.Up,
+                CellMovement.Down,
+                CellMovement.Left,
+                CellMovement.Right,
+            )
+            is Cell.EightDirectionCell -> CellMovement.values()
+        }
+        val validRange = 0 until BoardSize
+        return movements.filter { move ->
+            val x = this.id.first + move.direction.first
+            val y = this.id.second + move.direction.second
+            if (x !in validRange || y !in validRange) return@filter false
+            if (this@Board.cells[x][y].state != CellState.EMPTY) return@filter false
+            true
+        }
+    }
+
+    private fun copy(
+        cells: Array<Array<Cell>> = this.cells,
+        selectedCell: Cell? = this.selectedCell,
+    ) = Board(
+        cells = cells,
+        selectedCell = selectedCell,
+        initPieces = false,
+        currentTurn = this.currentTurn,
+    )
+
 }
 
-enum class CellState {
-    EMPTY,
-    WHITE_PAWN_OCCUPIED,
-    WHITE_KING_OCCUPIED,
-    BLACK_PAWN_OCCUPIED,
-    BLACK_KING_OCCUPIED
+enum class CellMovement(val direction: Pair<Int, Int>) {
+    Up(0 to -1),
+    Down(0 to 1),
+    Left(-1 to 0),
+    Right(1 to 0),
+    UpLeft(-1 to -1),
+    UpRight(1 to -1),
+    DownLeft(-1 to 1),
+    DownRight(1 to 1),
 }
 
-data class Cell(
-    internal var state: CellState = CellState.EMPTY,
-    internal var selected: Boolean = false,
-)
+enum class CellState(val type: Int) {
+    EMPTY(-1),
+    WHITE_PAWN_OCCUPIED(Board.WhiteTurn),
+    WHITE_KING_OCCUPIED(Board.WhiteTurn),
+    BLACK_PAWN_OCCUPIED(Board.BlackTurn),
+    BLACK_KING_OCCUPIED(Board.BlackTurn),
+}
+
+sealed class Cell(
+    val id: Pair<Int, Int>,
+    var state: CellState = CellState.EMPTY,
+    var hintState: CellState = CellState.EMPTY,
+) {
+    class FourDirectionCell(id: Pair<Int, Int>) : Cell(id)
+    class EightDirectionCell(id: Pair<Int, Int>) : Cell(id)
+}
